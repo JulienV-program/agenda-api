79fd812d71efe9fc86de1dd985647686830711eb
micfild <43708100+micfild@users.noreply.github.com>
2019-08-10 13:10:20 +0200

    fixed FreeTime Controller

--- src/Controller/FreeTimeHandler.php
+++ src/Controller/FreeTimeHandler.php
@@ -4,51 +4,54 @@
 namespace App\Controller;
 
 
-
 use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FreeTimeHandler extends AbstractController
 {
-public function handle($data){
-    $entityManager = $this->getDoctrine()->getManager();
+    public function handle($data)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
 
 //    dump($data);
 
 
 //    $startDay = new \DateTime('2019-08-08');
 //    $endDay = new \DateTime('2019-08-09');
-    $startDay = new \DateTime($data['start']);
-    $endDay = new \DateTime($data['end']);
+        $startDay = new \DateTime($data['start']);
+        $endDay = new \DateTime($data['end']);
 
-    $repository = $entityManager->getRepository(Event::class);
-    $eventByDate = $repository->findByDay($startDay, $endDay);
-    //        dump($startDay);
-    //        dump($endDay);
-
-
+        $repository = $entityManager->getRepository(Event::class);
+        $eventByDate = $repository->findByDay($startDay, $endDay);
+        //        dump($startDay);
+        //        dump($endDay);
 
 
-    $free = [];
-    $len = count($eventByDate);
-    foreach ($eventByDate as $index=>$item) {
-        if ($index === 0 && $item->getStart()->getDateTime() > $startDay)
-        {
-            $free[] = ["start" => $startDay, "end" => $item->getStart()->getDateTime() ];
+        $free = [];
+        $len = count($eventByDate);
+        foreach ($eventByDate as $index => $item) {
+            if ($index === 0 && $item->getStart()->getDateTime() > $startDay) {
+                $free[] = ["start" => $startDay, "end" => $item->getStart()->getDateTime()];
+            } elseif (($index > 0 && $index < $len - 1) && $item->getStart()->getDateTime() > $eventByDate[$index - 1]->getEnd()->getDateTime()) {
+                $free[] = [
+                    "start" => $eventByDate[$index - 1]->getEnd()->getDateTime(),
+                    "end" => $item->getStart()->getDateTime()
+                ];
+            } elseif ($index === ($len - 1) && $item->getEnd()->getDateTime() < $endDay) {
+                if ($item->getStart()->getDateTime() > $eventByDate[$index - 1]->getEnd()->getDateTime()) {
+
+                    $free[] = [
+                        "start" => $eventByDate[$index - 1]->getEnd()->getDateTime(),
+                        "end" => $item->getStart()->getDateTime()
+                    ];
+
+                }
+
+                $free[] = ["start" => $item->getEnd()->getDateTime(), "end" => $endDay];
+            }
         }
 
-        elseif ($index > 0 && $item->getStart()->getDateTime() > $eventByDate[$index - 1]->getEnd()->getDateTime())
-        {
-            $free[] = ["start" => $eventByDate[$index - 1]->getEnd()->getDateTime(), "end" => $item->getStart()->getDateTime() ];
-         }
 
-        elseif ($index === $len - 1 && $item->getEnd()->getDateTime() < $endDay)
-        {
-            $free[] = ["start" => $item->getEnd()->getDateTime(), "end" => $endDay ];
-        }
+        return $free;
     }
-
-
-return $free;
-}
 }
